ce3a7b0aaa3e818bbe0767fc466af0355edabbcc
Timo Weggen <timo.weggen@gmail.com>
2024-07-27 13:02:21 +0200

    - signing in with username/password finally working.

--- exli/lib/exli_web/controllers/random_controller.ex
+++ exli/lib/exli_web/controllers/random_controller.ex
@@ -1,7 +1,13 @@
 defmodule ExliWeb.RandomController do
   use ExliWeb, :controller
+  alias Exli.Accounts
 
   def index(conn, _params) do
-    send_resp(conn, 200, "Exli all!")
+    [token64] = get_req_header(conn, "x-nassau-token")
+    IO.inspect("Reading token header ")
+    IO.inspect(token64)
+    {_, token} = Base.url_decode64(token64, padding: false)
+    user = Accounts.get_user_by_session_token(token)
+    send_resp(conn, 200, "Exli all for #{user.email}!")
   end
 end
